fd033d780b7299e3c7657f23b0770cb11c2839b6
wanlong1234567 <2531165965@qq.com>
2019-06-23 18:39:22 +0800

    test

--- src/main/java/com/wl/day01/day01.java
+++ src/main/java/com/wl/day01/day01.java
@@ -2,6 +2,8 @@ package com.wl.day01;
 
 public class day01 {
     public static void main(String[] args) {
+        System.out.println("args11 = [" + args + "]");
         System.out.println("args = [" + args + "]");
+        System.out.println("argddds = [" + args + "]");
     }
 }
